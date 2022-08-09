5a52f6a07762da24e4fb296e7a4119379eef497a
Vincent <v3rdugo@yahoo.com>
2022-08-09 15:13:10 -0700

    basic inheritance from openzeppelin ERC721 standard

--- contracts/BasicNFT.sol
+++ contracts/BasicNFT.sol
@@ -1,7 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract BasicNFT
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721
 {
-    
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG")
+    {
+        s_tokenCounter = 0;
+    }
+
+    function mintNFT() public returns(uint256)
+    {
+        // mint the token to whoever calls this mint function
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
+        return s_tokenCounter;
+    }
+
+    function getTokenCounter() public view returns(uint256)
+    {
+        return s_tokenCounter;
+    }
 }
