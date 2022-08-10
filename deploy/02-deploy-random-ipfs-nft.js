e9aa56878128ccf4a33ec22e92c8a36571cc1627
Vincent <v3rdugo@yahoo.com>
2022-08-10 15:23:16 -0700

    set up deploy mocks and random ipfs nft scripts

---
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,32 @@
+const { network, ethers } = require('hardhat')
+const { developmentChains, networkConfig } = require('../helper-hardhat-config')
+const { verify } = require('../utils/verify')
+
+module.exports = async ({getNamedAccounts, deployments}) =>
+{
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (!developmentChains.includes(network.name))
+    {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    }
+    else
+    {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    log('--------------------------------------------')
+    const args = [vrfCoordinatorV2Address, subscriptionId, networkConfig[chainId].gasLane, networkConfig[chainId].mintFee, networkConfig[chainId].callbackGasLimit]
+    
+}
+
+module.exports.tags = ["all", "randomipfsnft", "main"]
