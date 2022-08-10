194bdea38641c5190fa66848aa5aad9fba2783d2
Vincent <v3rdugo@yahoo.com>
2022-08-10 14:56:04 -0700

    fix import problem

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
