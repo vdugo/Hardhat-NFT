60829f0400f4b53abb84d46c3ede087415ada460
Vincent <v3rdugo@yahoo.com>
2022-08-10 21:24:49 -0700

    set up boilerplate

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// will be stored completely on-chain, use regular ERC721
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract DynamicSvgNft
+contract DynamicSvgNft is ERC721
 {
     // mint
     // store our svg information somewhere
     // some logic to say 'show x image' or 'show y image'
+
+    uint256 private s_tokenCounter;
+
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN")
+    {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public
+    {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
+    }
 }
