1a32cd6247b365461840c6701e4c6322bf77c753
Vincent <v3rdugo@yahoo.com>
2022-08-11 16:57:34 -0700

    all deployments working on localhost

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -31,16 +31,20 @@ contract DynamicSvgNft is ERC721
 
     function svgToImageURI(string memory svg) public pure returns(string memory)
     {
+        // example:
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
+        // would return ""
+        string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 
     function mintNft(int256 highValue) public
     {
         s_tokenIdToHighValues[s_tokenCounter] = highValue;
-        s_tokenCounter += 1;
-        _safeMint(msg.sender, s_tokenCounter);
         emit CreatedNFT(s_tokenCounter, highValue);
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
     }
 
     function _baseURI() internal pure override returns(string memory)
