679bdfce2ea16d74f3c793c28c3d52d3bb2c4149
Vincent <v3rdugo@yahoo.com>
2022-08-11 16:01:10 -0700

    add mock script and deploy for dynamic svg nft

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -4,6 +4,9 @@ const { developmentChains } = require('../helper-hardhat-config')
 const BASE_FEE = ethers.utils.parseEther("0.25") // costs 0.25 LINK per request
 const GAS_PRICE_LINK = 1e9 // LINK per gas
 
+const DECIMALS = 18
+const INITIAL_PRICE = ethers.utils.parseUnits("2000", "ether")
+
 module.exports = async({ getNamedAccounts, deployments }) =>
 {
     const { deploy, log } = deployments
@@ -19,6 +22,12 @@ module.exports = async({ getNamedAccounts, deployments }) =>
             from: deployer,
             log: true,
             args: args,
+        })
+
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE]
         })
         log("Mocks deployed")
         log("------------------------------------------------------")
