5f3fdea88cdeea782fecdf2ea700b9d5f89e9153
Vincent <v3rdugo@yahoo.com>
2022-08-10 21:20:05 -0700

    these NFTs will not use URI, they will be completely on chain, use regular ERC721

---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DynamicSvgNft
+{
+    // mint
+    // store our svg information somewhere
+    // some logic to say 'show x image' or 'show y image'
+}
